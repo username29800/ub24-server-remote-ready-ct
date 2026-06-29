cb87341528743d8b612deeef6cc4a578ca69bceb
username29800 <piapia78490@gmail.com>
2026-06-29 14:02:50 +0900

    mod: rebuild build script with variable-centric configuration method

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
-sed -i "1cFROM ${1}:latest" */Dockerfile
-sed -i "1cFROM docker.io/library/debian:${2}" base/Dockerfile
-podman build -t "$1" base --no-cache
-podman build -t "$1" ssh --no-cache
-podman build -t "$1" dev --no-cache
-podman build -t "$1" gui --no-cache
-podman build -t "$1" agent --no-cache
+#configuration
+builder="podman build"
+baseimg="docker.io/library/debian"
+buildbase="latest"
+flags="--no-cache"
+
+#runner
+sed -i "1cFROM ${1}:$buildbase" */Dockerfile
+sed -i "1cFROM $baseimg:${2}" base/Dockerfile
+$builder -t "$1" base $flags
+$builder -t "$1" ssh $flags
+$builder -t "$1" dev $flags
+$builder -t "$1" gui $flags
+$builder -t "$1" agent $flags
+
--- header.sh
+++ header.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
-sed -i "1cFROM ${1}:latest" */Dockerfile
-sed -i "1cFROM docker.io/library/debian:${2}" base/Dockerfile
+#configuration
+builder="podman build"
+baseimg="docker.io/library/debian"
+buildbase="latest"
+flags="--no-cache"
+
+#runner
+sed -i "1cFROM ${1}:$buildbase" */Dockerfile
+sed -i "1cFROM $baseimg:${2}" base/Dockerfile
+
