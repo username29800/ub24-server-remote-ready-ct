a750637e2a18dfc4c6c6c7122d4bc12316e8dbe0
username29800 <piapia78490@gmail.com>
2026-07-23 13:16:35 +0900

    add fallback line in apps/internet.sh segment

--- segments/apps/internet.sh
+++ segments/apps/internet.sh
@@ -4,6 +4,7 @@
 arc_discord='https://stable.dl2.discordapp.net/apps/linux/1.0.150/discord-1.0.150.deb'
 
 apt install -y firefox-esr thunderbird purple-discord pidgin hexchat irssi w3m lynx elinks
+apt install -y thunderbird purple-discord pidgin hexchat irssi w3m lynx elinks
 
 curl -o nedcat-install-Discord.deb "$arc_discord"
 apt install -y ./nedcat-install-Discord.deb
